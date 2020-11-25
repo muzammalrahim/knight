6f84e770b3dbcce7405a0ee8972d3b0d75680d80
Arslan Mehmood <arslanmehmood051@gmail.com>
2020-11-25 11:44:38 +0500

    Login with email and password

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 // import axios from "axios";
-// import * as _redux from "./redux";
+import * as _redux from "./redux";
 import store, { persistor } from "./redux/store";
 import App from "./app/App";
 import "./index.scss"; // Standard version
